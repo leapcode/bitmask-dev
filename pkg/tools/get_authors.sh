9b889f8789a77a1457e5cb5724aed908ba42af67
varac <varacanero@zeromail.org>
2017-05-31 16:29:49 +0200

    [docs] use mailmap to de-duplicate committers

--- pkg/tools/get_authors.sh
+++ pkg/tools/get_authors.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-git log --format='%aN <%aE>' | awk '{arr[$0]++} END{for (i in arr){print arr[i], i;}}' | sort -rn | cut -d' ' -f2-
+#
+# To de-duplicate dontibutors who committed with different email addresses / names you can link
+# ~/.mailmap to the .mailmap in the `secrets` repo.
+# You need to tell git where to find it:
+#
+#     git config --global mailmap.file ~/.mailmap
+#
+# After this just run `./get_authors.sh`.
+
+git log --use-mailmap --format='%aN <%aE>' | awk '{arr[$0]++} END{for (i in arr){print arr[i], i;}}' | sort -rn | cut -d' ' -f2- | sort
