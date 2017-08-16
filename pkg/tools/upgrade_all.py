7d497583461639640d211b7a3dfc34931e8f3e54
Kali Kaneko <kali@leap.se>
2017-08-15 22:14:24 -0400

    pep8

--- pkg/tools/upgrade_all.py
+++ pkg/tools/upgrade_all.py
@@ -5,8 +5,8 @@ import pip
 from subprocess import call
 
 if not os.environ.get('VIRTUAL_ENV'):
-    print('[!] Should call this script inside a virtualenv, I do not want to mess '
-          'with your system. Bye!')
+    print('[!] Should call this script inside a virtualenv, I do not want to '
+          'mess with your system. Bye!')
     sys.exit(1)
 
 for dist in pip.get_installed_distributions():
