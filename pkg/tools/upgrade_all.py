34d59f070d13ec2ac09c07cc7e0ec6a5d559fef9
Kali Kaneko <kali@leap.se>
2017-08-23 01:30:33 -0400

    [bug] fix bad merge

--- pkg/tools/upgrade_all.py
+++ pkg/tools/upgrade_all.py
@@ -5,13 +5,8 @@ import pip
 from subprocess import call
 
 if not os.environ.get('VIRTUAL_ENV'):
-<<<<<<< HEAD
     print('[!] Should call this script inside a virtualenv, I do not want to '
           'mess with your system. Bye!')
-=======
-    print('[!] Should call this script inside a virtualenv, I do not want to mess '
-          'with your system. Bye!')
->>>>>>> 135aa3ac... [docs] add ability to upgrade an existing virtualenv
     sys.exit(1)
 
 for dist in pip.get_installed_distributions():
