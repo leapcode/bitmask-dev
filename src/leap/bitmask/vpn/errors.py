cef98e7a4b00bca59289e0c447c9e9de88503bf5
Kali Kaneko <kali@leap.se>
2017-08-25 12:00:32 -0400

    [refactor] remove error import

--- src/leap/bitmask/vpn/errors.py
+++ src/leap/bitmask/vpn/errors.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ._management import OpenVPNAlreadyRunning, AlienOpenVPNAlreadyRunning
 from .launcher import OpenVPNNotFoundException, VPNLauncherException
 from leap.bitmask.vpn.launchers.linux import (
     NoPolkitAuthAgentAvailable, NoPkexecAvailable)
 from leap.bitmask.vpn.launchers.darwin import NoTunKextLoaded
 
 
-__all__ = ["OpenVPNAlreadyRunning", "AlienOpenVPNAlreadyRunning",
-           "OpenVPNNotFoundException", "VPNLauncherException",
+__all__ = ["OpenVPNNotFoundException", "VPNLauncherException",
            "NoPolkitAuthAgentAvailable", "NoPkexecAvailable",
            "NoTunKextLoaded"]
