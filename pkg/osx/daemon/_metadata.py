8f826342f88cd977738d6a2937031d1bdef682d4
Kali Kaneko <kali@leap.se>
2018-09-29 23:17:20 +0200

    [pkg] remove osx pkg folder

--- pkg/osx/daemon/_metadata.py
+++
@@ -1,7 +0,0 @@
-author_name = "Ben Finney"
-author_email = "ben+python@benfinney.id.au"
-year_range = "2001-2017"
-copyright = "Copyright © {year_range} {author} and others".format(
-    year_range=copyright_year_range, author=author)
-license = "Apache-2"
-url = "https://alioth.debian.org/projects/python-daemon/"
--- pkg/osx/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-HELPER_PLIST="/Library/LaunchDaemons/se.leap.bitmask-helper.plist"
-sudo launchctl unload $HELPER_PLIST
-sudo rm -rf /Applications/Bitmask.app
-sudo rm -rf ~/Library/Preferences/leap
-sudo rm $HELPER_PLIST
-echo "Bitmask has been uninstalled from your system!"
